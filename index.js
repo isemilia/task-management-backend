01d84e432b1ef25f17ee89eac7f12a203697ad33
Emilia <81705094+emiliaweb@users.noreply.github.com>
2024-01-07 13:57:52 +0600

    Refactor user creation

--- index.js
+++ index.js
@@ -28,49 +28,50 @@ app.get('/', (req, res) => {
 });
 
 app.post('/auth/signup', signupValidation, async (req, res) => {
-  const errors = validationResult(req);
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      isSuccess: false,
-      data: {},
-      info: {
-        message: 'Validation failed',
-        details: errors.array()
-      }
-    });
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-  const user = new UserModel({
-    username: req.body.username,
-    password: hashedPassword,
-    name: req.body.name,
-    avatar: req.body.avatar,
-  });
-
-  await user.save()
-    .then(() => {
-      res.json({
-        isSuccess: true,
-        data: {},
-        info: {}
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.json({
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
         isSuccess: false,
         data: {},
         info: {
-          message: 'Failed to create user',
-          details: null
+          message: 'Validation failed',
+          details: errors.array()
         }
-      })
-    })
+      });
+    }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new UserModel({
+      username: req.body.username,
+      password: hashedPassword,
+      name: req.body.name,
+      avatar: req.body.avatar,
+    });
+
+    await user.save()
+      .then(() => {
+        res.json({
+          isSuccess: true,
+          data: {},
+          info: {}
+        });
+      })
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      isSuccess: false,
+      data: {},
+      info: {
+        message: 'Failed to create user',
+        details: null
+      }
+    });
+  }
 
 });
 
