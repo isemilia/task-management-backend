2547bf739f31288b88fd095c6e995368b075170b
Emilia <81705094+emiliaweb@users.noreply.github.com>
2024-01-09 22:51:25 +0600

    Introduce task creation

---
+++ models/Task.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  labels: {
+    type: Array,
+    default: []
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  }
+}, { timestamps: true });
+
+export default mongoose.model('task', TaskSchema);
