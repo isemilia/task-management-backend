30234d81d81ed55d47711c92169d760e8cafb3b6
Emilia <81705094+emiliaweb@users.noreply.github.com>
2024-01-10 22:42:12 +0600

    Connect to database in server.ts

--- utils/server.ts
+++ utils/server.ts
@@ -1,5 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
 import router from './endpoints'
+
+if(process.env.DATABASE_URI === undefined){
+  throw Error('env DATABASE_URI is undefined')
+}
+mongoose
+  .connect(process.env.DATABASE_URI)
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
 
 const app = express();
 
