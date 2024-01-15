b767035c9b750ef9783cbf75b1624eae3c40a265
Emilia <81705094+emiliaweb@users.noreply.github.com>
2024-01-15 21:54:57 +0600

    Update task

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express'
 import {loginValidation, signupValidation} from '../validations/auth'
 import checkAuth from '../utils/checkAuth'
 import UserController from '../controllers/UserController';
-import TaskController, {getAllByCurrentUser} from '../controllers/TaskController'
+import TaskController from '../controllers/TaskController'
 import {createTaskValidation} from '../validations/task';
 
 const router = express.Router()
@@ -14,9 +14,7 @@ router.get('/', async (_req: Request, res: Response) => {
 
 // user routs
 router.post('/auth/login', loginValidation, UserController.login);
-
 router.post('/auth/signup', signupValidation, UserController.signup);
-
 router.get('/auth/me', checkAuth, UserController.getMe);
 
 // task routes
@@ -24,5 +22,6 @@ router.post('/tasks', checkAuth, ...createTaskValidation, TaskController.create)
 router.get('/tasks', checkAuth, TaskController.getAllByCurrentUser);
 router.get('/tasks/:id', checkAuth, TaskController.getOne);
 router.delete('/tasks/:id', checkAuth, TaskController.remove);
+router.patch('/tasks/:id', checkAuth, TaskController.updateOne);
 
 export default router
