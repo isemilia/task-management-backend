f679c3d8f41382ad3616e44af98633dd97f1ba9b
Emilia <81705094+emiliaweb@users.noreply.github.com>
2024-01-21 13:16:38 +0600

    Adjust validation

--- validations/auth.ts
+++ validations/auth.ts
@@ -2,12 +2,12 @@ import { body } from "express-validator";
 
 export const signupValidation = [
   body('name').isLength({ min: 2 }),
-  body('username').isLength({ min: 6 }),
+  body('username').isLength({ min: 3 }),
   body('password').isLength({ min: 6 }),
   body('avatar').optional().isURL(),
 ];
 
 export const loginValidation = [
-  body('username').isLength({ min: 6 }),
+  body('username').isLength({ min: 3 }),
   body('password').isLength({ min: 6 })
 ];
