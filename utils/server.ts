a6cdf3216b07d2ab6f8d261bd970046cc2eb36e6
Emilia <81705094+isemilia@users.noreply.github.com>
2024-03-08 17:01:29 +0500

    Config for vercel

--- utils/server.ts
+++ utils/server.ts
@@ -8,6 +8,7 @@ import router from './endpoints'
 if(process.env.MONGODB_URI === undefined){
   throw Error('env MONGODB_URI is undefined')
 }
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -24,17 +25,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true
 }));
 
-// app.use((req, res, next) => {
-//   console.log(req.cookies)
-//   next();
-// })
-
 app.use(router);
-
 
 app.listen(4000, () => {
   console.log('Server is running at port 4000');
