af36941cf2541035aacc137ef24e544445d07ca2
Emilia <81705094+isemilia@users.noreply.github.com>
2024-03-10 08:21:10 +0500

    Clean up

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -27,7 +27,6 @@ app.use(cookieParser());
 const allowedOrigins = ['http://localhost:5173', 'https://tasks-isemilia.vercel.app']
 app.use(cors({
   origin: (requestOrigin, checkOrigin) => {
-    console.log('Request origin:', requestOrigin)
     if (!requestOrigin || allowedOrigins.includes(requestOrigin)) {
       checkOrigin(null, true)
     } else {
